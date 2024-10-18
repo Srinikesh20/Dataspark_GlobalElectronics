246986f3cd93b73a2b1e36cca15668714777ad02
Srinikesh20 <nikinikesh2@gmail.com>
2024-10-18 14:50:15 +0530

    Update Product analysis.sql

--- Product analysis.sql
+++ Product analysis.sql
@@ -3,20 +3,20 @@
 SELECT
     category,subcategory,
     ROUND(SUM(unit_price_usd * quantity),2) AS total_sales
-FROM overall
+FROM data_spark.overall
 GROUP BY category,subcategory
 ORDER BY total_sales DESC;
 
 -- Product Popularity: Identify the most and least popular products based on sales data.
 
 select distinct(product_name),sum(quantity) as Quantity
-from overall
+from data_spark.overall
 group by product_name
 order by Quantity desc
 limit 10
 
 select distinct(product_name),sum(quantity) as Quantity
-from overall
+from data_spark.overall
 group by product_name
 order by Quantity 
 limit 10
@@ -26,7 +26,7 @@ limit 10
 Select
 product_name,
 ROUND(Sum((unit_price_usd-unit_cost_usd)*quantity),2) as Profit_Margin
-from overall
+from data_spark.overall
 group by product_name
 order by Profit_Margin desc
 limit 10
